1fb87ae2b2a9d405aa71fdf36205c44e6fb34276
Tom Harrison <tjharrisonuk@gmail.com>
2020-05-30 14:42:05 +0100

    barebones start of the meet class

---
+++ model/Meet.php
@@ -0,0 +1,47 @@
+<?php
+
+class MeetException extends Exception {}
+
+class Meet {
+
+    private $_id;
+    private $_title; //eg. Tom's Birthday Drinks
+    private $_description;
+    private $_scheduledTime; //a scheduled time that can be adjusted
+    private $_finalised; //boolean has the event been confirmed
+    private $_organiser; //a user object of id
+    private $_attendees = array(); //an array of user objects or ids
+
+
+    /* Getters */
+
+    public function getID(){
+        return $this->_id;
+    }
+
+    public function getTitle(){
+        return $this->_title;
+    }
+
+    public function getDescription(){
+        return $this->_description;
+    }
+
+    public function getScheduledTime(){
+        return $this->_scheduledTime;
+    }
+
+    public function getFinalised(){
+        return $this->_finalised;
+    }
+
+    public function getOrganiser(){
+        return $this->_organiser;
+    }
+
+    public function getAttendees(){
+        return $this->_attendees;
+    }
+
+
+}
