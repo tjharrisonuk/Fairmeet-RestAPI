31be9ef1882cee736f6e5e2344a0042969c4564e
Tom Harrison <tjharrisonuk@gmail.com>
2020-05-31 14:46:41 +0100

    meet setters

--- model/Meet.php
+++ model/Meet.php
@@ -44,6 +44,46 @@ class Meet {
     }
 
     /* Setters including database validation*/
+    public function setId($id){
+
+        // Can't be null
+        // Must be numeric
+        // Can't have a negative value
+        // Cant be greater than BIGINT
+        // Id can't have already been set
+
+        if(($id !== null) && (!is_numeric($id) || $id <= 0 || $id > 9223372036854775807 || $this->_id !== null)){
+            throw new MeetException("Meet Id Error");
+        }
+        $this->_id = $id;
+    }
+
+    public function setTitle($title){
+        if(strlen($title) < 0 || strlen($title) > 255){
+            throw new TaskException("Meet title error");
+        }
+
+        $this->_title = $title;
+    }
+
+    public function setDescription($description){
+        if(($description !== null) && (strlen($description) > 147772145)){
+            throw new TaskException("Task description error");
+        }
+
+        $this->_description = $description;
+    }
+
+    public function setScheduledTime($scheduledTime){
+
+        if(($scheduledTime !== null) && date_format(date_create_from_format('d/m/Y H:i', $scheduledTime), 'd/m/Y H:i') != $scheduledTime){
+            throw new TaskException("Meet Scheduled Time error");
+        }
+
+        $this->_scheduledTime = $scheduledTime;
+
+    }
+
 
 
 }
