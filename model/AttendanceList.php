883b5aa836d9dc1b9ff96d2f04d640318a64174b
Tom Harrison <tjharrisonuk@gmail.com>
2020-06-19 20:43:07 +0100

    start of attendance class

---
+++ model/AttendanceList.php
@@ -0,0 +1,27 @@
+<?php
+namespace fairmeet\model;
+use Exception;
+class MeetException extends Exception {}
+
+class attendanceList {
+
+    //meet id
+    private _meetid;
+    private _userIdArray;
+    //attendance userid array
+
+
+    //methods
+
+    //constructor
+        //including database queries to
+
+
+
+    //add attendee
+        //need to check on invitation to meet
+    //remove attendee
+        //can be done by either the attendee or the organiser
+
+}
+
